269da7cc5ac857f6dac35d1dd3c93041c5b2390d
Fe-Lydia <709258439@qq.com>
2017-11-05 23:24:05 +0800

    退出登录完成

--- public/src/utils.js
+++ public/src/utils.js
@@ -1,13 +1,26 @@
 define(['jquery'], function($) {
     //判断员工是否登录 发送请求
     $.ajax({
-        // /api http://localhost:3000
-        url: '/api/employee/checkRootLogin',
-        type: 'get',
-        success: function(info) {
-            if (info.error) {
-                // location.href = '/login.html';
+            // /api http://localhost:3000
+            url: '/api/employee/checkRootLogin',
+            type: 'get',
+            success: function(info) {
+                if (info.error) {
+                    location.href = '/login.html';
+                }
             }
-        }
+        })
+        //退出
+    $('.logout').on('click', function() {
+        $.ajax({
+            url: '/api/employee/employeeLogout',
+            type: 'get',
+            success: function(info) {
+                if (info.success) {
+                    location.href = '/login.html';
+                }
+            }
+        })
     })
+
 });
